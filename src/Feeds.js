a7aedb2fc010f24d7f4e50e43b83e3d45252cc23
Joelle Johnson <joelle.johnson@dcmail.ca>
2022-04-08 19:01:54 -0400

    Working Stream from webcam

--- src/Feeds.js
+++ src/Feeds.js
@@ -1,43 +1,44 @@
 import React from 'react';
 import { useRef, useEffect, useState } from 'react';
 import useCanvas from './useCanvas';
-import testVid from './test_Vid/gameplay.mp4';
+// import usePhoto from './getPhoto';
+// import testVid from './test_Vid/gameplay.mp4';
 
 function Canvas(props) {
     const {draw, ...rest } = props;
     const canvasRef = useCanvas(draw);
+    // const videoRef = usePhoto();
 
     // ! To Move
     const videoRef = useRef(null);
     // const photoRef = useRef(null);
     const [hasPhoto, setHasPhoto] = useState(false);
     const getVideo = () => {
-        // navigator.mediaDevices.getUserMedia({ video: {width: 800, height: 450}})
-        // .then(stream => {
-        //     let video = videoRef.current;
-        //     video.srcObject = stream;
-        //     video.play()
-        // })
-        // .catch(err => {
-        //     console.error(err);
-        // });
-        let video = videoRef.current;
-        video.src = testVid;
-        video.play();
+        // Get stream from webcam
+        navigator.mediaDevices.getUserMedia({ video: {width: 800, height: 450}})
+        .then(stream => {
+            let video = videoRef.current;
+            video.srcObject = stream;
+            video.play()
+        })
+        .catch(err => {
+            console.error(err);
+        });
+        // ! Remove commented out below (this was for testing)
+        // let video = videoRef.current;
+        // video.src = testVid;
+        // video.play();
     }
     useEffect(() => {
         getVideo();
     }, [videoRef]);
 
-    // const takePhoto = () => {
-    //     const width = 800;
-    //     const height = 450;
-    //     let video = videoRef.current;
-    //     let photo = photoRef.current;
-    //     photo.width = width;
-    //     photo.height = height;
+    const takePhoto = () => {
+        const width = 800;
+        const height = 450;
+        let video = videoRef.current;
 
-    // }
+    }
 
     return(
         <section className="photoFeedSect">
@@ -48,7 +49,7 @@ function Canvas(props) {
                     // width="800" height="450" autoPlay muted loop
                     ref={videoRef}></video>
                 </div>
-                <div className={"playerDiv" + (hasPhoto ? "hasPhoto" : "")}>
+                <div className="playerDiv">
                     <h2 className="photoBoothTitle centerText">Effects Feed</h2>
                     <canvas id="canvas" className="photo"
                     ref={canvasRef} {...rest}></canvas>
